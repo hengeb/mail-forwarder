4bfb6ac0ccfc789db6d27d2f66c88442e34e3d28
Henrik Gebauer <code@henrik-gebauer.de>
2021-10-03 22:28:13 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # mail-forwarder
+
 forward mail from generic address to restricted accounts
+
+## Summary
+
+`mail-forwarder` lets you set up a generic mail address like `forward@example.org` for forwarding mails to undisclosed addresses or accounts with restricted access.
+
+In the example of `forward@example.org`, mails to `forward+username@example.org` would be forwarded to `username@another.example.org` where `another.example.org` is configurable.
+
+## Install
+
+Run `composer install`
+
+Copy `config.json.sample` to `config.json` and change it for your needs, see section *Configuration* below.
+
+Let a cron job run `php run.php` periodically.
+
+## Configuration
+
+`config.json` must be in valid JSON format.
+
+Compare the entries in `config.json.sample` with the example in the above section *Summary*.
+
+`source` contains the IMAP configuration (currently, only `"type": "imap"` is supported) for the generic mail address.
+
+`target` contains the configuration for the forwarding (currently, only `"type": "smtp"` is supported).
+
+You can set up a list of trusted mail addresses that are allowed to send mails to the forwarding service. Leave the list empty to allow all incoming mails. Mails from any other origin will be reported to the `abuseAddress`.
---
+++ run.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * mail forwarder
+ * 
+ * @author Henrik Gebauer <code@henrik-gebauer.de>
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'vendor/autoload.php';
+
+$config = json_decode(file_get_contents('config.json'), true);
+
+$inbox = new PhpImap\Mailbox(
+	"{{$config['source']['host']}:{$config['source']['port']}/imap/{$config['source']['secure']}}{$config['source']['folder']}",
+	$config['source']['user'],
+	$config['source']['password']
+);
+
+$inbox->setConnectionArgs(
+    CL_EXPUNGE // expunge deleted mails upon mailbox close
+    | OP_SECURE // don't do non-secure authentication
+);
+
+try {
+	$mailsIds = $inbox->searchMailbox('ALL');
+} catch(PhpImap\Exceptions\ConnectionException $ex) {
+	echo "IMAP connection failed: " . $ex;
+	die();
+}
+
+$outbox = new PHPMailer();
+$outbox->isSMTP();
+$outbox->Host = $config['target']['host'];
+$outbox->SMTPAuth = true;
+$outbox->Username = $config['target']['user'];
+$outbox->Password = $config['target']['password'];
+$outbox->SMTPSecure = $config['target']['secure'];
+$outbox->Port = $config['target']['port'];
+$outbox->CharSet = "UTF-8";
+
+foreach ($mailsIds as $mailId) {
+    $outbox->clearAddresses();
+    $outbox->clearReplyTos();
+    $outbox->clearCustomHeaders();
+
+    $forwardedFor = [];
+
+    $mail = $inbox->getMail($mailId);
+
+    $outbox->setFrom($config['target']['senderAddress'], $mail->senderName);
+    
+    foreach ($mail->to as $address=>$name) {
+        preg_match('/^[^\+]+\+([^@]+)@/', $address, $matches);
+        $outbox->addAddress($matches[1] . "@" . $config['target']['domain'], $name);
+        $forwardedFor[] = $matches[1];
+    }
+    $outbox->Subject = $mail->subject;
+    $outbox->MessageID = $mail->messageId;
+    $outbox->MessageDate = $mail->headers->date;
+    $outbox->addCustomHeader('X-Forwarded-From', $mail->senderAddress);
+    $outbox->addCustomHeader('X-Forwarded-For', implode(', ', $forwardedFor));
+
+    $headers = parseHeaders($mail->headersRaw);
+    foreach ($headers as $name => &$value) {
+        if (in_array($name, [
+            'List-Id', 'List-Help', 'X-Course-Id', 'X-Course-Name', 'Precedence',
+            'X-Auto-Response-Suppress', 'Auto-Submitted', 'List-Unsubscribe',
+            'Thread-Topic', 'Thread-Index', 'In-Reply-To', 'Reply-To'
+        ], true)) {
+            $value = $inbox->decodeMimeStr($value);
+            $outbox->addCustomHeader($name, $value);
+        }
+    }
+
+    if ($mail->textHtml) {
+        $outbox->isHTML(true);
+        $outbox->Body = $mail->textHtml;
+        $outbox->AltBody = $mail->textPlain;
+    } else {
+        $outbox->isHTML(false);
+        $outbox->Body = $mail->textPlain;
+        $outbox->AltBody = '';
+    }
+    
+    if ($config['allowedSenderAddresses'] && !in_array($mail->senderAddress, $config['allowedSenderAddresses'], true)) {
+        $outbox->Subject = "mail rejected: " . $outbox->Subject;
+        $outbox->clearAddresses();
+        $outbox->addAddress($config['abuseAddress']);
+    }
+
+    if ($outbox->send()) {
+        $inbox->deleteMail($mailId);
+    } else {
+        echo $outbox->ErrorInfo . "\n";
+    }
+}
+
+$inbox->disconnect();
+$outbox->smtpClose();
+
+function parseHeaders(string $header): array
+{
+    $headers = [];
+    $name = '';
+    foreach (explode("\n", trim($header)) as $line) {
+        if ($line[0] !== " " && $line[0] !== "\t") {
+            list($name, $value) = explode(':', $line, 2);
+            $headers[$name] = trim($value);
+        } else {
+            $headers[$name] .= "\n" . $line;
+        }
+    }
+    return $headers;
+}
+
+echo "done\n";
